4b5dfd3a7d37d81b91f702360f0c38b90fb7b77f
Sydney <sydneyparsonsyb@gmail.com>
2021-08-24 13:09:11 -0400

    setup

---
+++ frontend/todo_redux.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = document.getElementById('root');
+
+    ReactDOM.render(
+        <h1>Todos App</h1>, root
+    )
+})
+
